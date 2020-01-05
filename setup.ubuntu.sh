5d157be0a36d61587f513a6800e2a1c7bbbfc015
qh73xe <takuya.waseda.1119@gmail.com>
2020-01-06 03:42:20 +0900

    cmigemo の追加

--- setup.ubuntu.sh
+++ setup.ubuntu.sh
@@ -1,5 +1,5 @@
 if type "apt" > /dev/null 2>&1; then
-    sudo apt install ctags global mupdf xdotool chktex jq shellcheck
+    sudo apt install ctags global mupdf xdotool chktex jq shellcheck cmigemo
 else
     echo "apt is not this pc!!"
 fi
@@ -9,7 +9,7 @@ else
     echo "pip3 is not this pc!!"
 fi
 if type "yarn" > /dev/null 2>&1; then
-    yarn add glob neovim eslint tslint prettier stylelint sasslint
+    yarn add -G neovim eslint tslint prettier stylelint sasslint
 else
     echo "yarn is not this pc!!"
 fi
